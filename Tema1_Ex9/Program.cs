ef43b38638a3c6c7f11de3f7b30bc5e05c3b1d2b
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:04:08 +0200

    Add project files.

---
+++ Tema1_Ex9/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Ex 9
+Scrieti un program care interschimba valoarea a doua variabile intregi.*/
+
+Console.WriteLine("Introduceti prima valoare:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduceti a doua valoare:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Valori initiale:");
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+
+// interschimbare
+int temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine("Valori după interschimbare:");
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
